85195ced982c264e280ee459a9376a0b90cbe46a
Michele Barbosa <michellebarboza82@gmail.com>
2025-06-03 13:22:53 +0000

    feat: solução da tarefa - tipos

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 // Escreva seu código aqui!
+let quantidadePortas: number = 2;
+let nomeColegaAoLado: string = "Mariana";
+let aulaAtiva: boolean = false;
+let quadroBranco: undefined = undefined;
+let salaDeAula: object = {
+    laboratorio: 3,
+    bloco: 1,
+    temArCondicionado: true
+};
+
+console.log("Quantidade de portas:", quantidadePortas);
+console.log("Nome da colega ao meu lado", nomeColegaAoLado);
+console.log("A aula está ativa?", aulaAtiva);
+console.log("quadroBranco", quadroBranco);
+console.log("informações da sala de aula", salaDeAula);
